c4ba713434133423ddb56e32c38bdecad1c9b3f4
omwitsa-etole <omwitsaetole@gmail.com>
2026-05-29 14:41:12 +0300

    home index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -128,6 +128,7 @@ namespace SACCOBlockChainSystem.Controllers
                 // Handle Member role - redirect to MemberIndex
                 if (userRole?.ToUpper() == "MEMBER")
                 {
+                    //ensure here to final
                     var uid = User.FindFirst("UserId")?.Value;
                     
                     var wallet = await _context.Wallets.FirstOrDefaultAsync(w => w.MemberId == int.Parse(uid) && w.CompanyCode == userCompanyCode);
@@ -157,6 +158,7 @@ namespace SACCOBlockChainSystem.Controllers
                             }
                         }
                     }
+                    //to here
                     var trs = await _context.BlockchainTransactions.AsNoTracking().OrderByDescending(t => t.CreatedAt).Where(t => t.CompanyCode == member.CompanyCode && t.MemberNo == member.MemberNo).Take(20).ToListAsync();
                     var memberView = new MemberViewModel {
                         Member = member,
